1ca2b803c230182d4154bdf6142c3c663d3be1c1
Val Grace <gracevalerie1@gmail.com>
2024-11-10 10:34:54 +0300

    Password bruteforce to unzip file locked with ransomware

---
+++ .history/bruteforce_20241110100949.py
@@ -0,0 +1,35 @@
+'''
+Forage AIG Cybersecurity Program
+Bruteforce starter template
+'''
+
+from zipfile import ZipFile
+
+# Use a method to attempt to extract the zip file with a given password
+# def attempt_extract(zf_handle, password):
+#     
+#
+#
+
+def main():
+    print("[+] Beginning bruteforce ")
+    with ZipFile('enc.zip') as zf:
+        with open('rockyou.txt', 'rb') as f:
+            # Write your logic here...
+            # Iterate through password entries in rockyou.txt
+            contents = f.read()
+            pwds = contents.split()
+            pwdlist= [str(value) for value in pwds]
+            for fx in pwdlist:
+                zf.extractall("../EncryptedFilePack", pwd=fx)
+
+                print(f"Successfully extracted files")
+
+            # Attempt to extract the zip file using each password
+
+            # Handle correct password extract versus incorrect password attempt)
+
+    #print("[+] Password not found in list")
+
+if __name__ == "__main__":
+    main()
---
+++ .history/bruteforce_20241110102730.py
@@ -0,0 +1,42 @@
+'''
+Forage AIG Cybersecurity Program
+Bruteforce starter template
+'''
+
+from zipfile import ZipFile
+
+# Use a method to attempt to extract the zip file with a given password
+# def attempt_extract(zf_handle, password):
+#     
+#
+#
+
+def main():
+    print("[+] Beginning bruteforce ")
+    with ZipFile('enc.zip') as zf:
+        with open('rockyou.txt', 'r') as f:
+            # Write your logic here...
+            # Iterate through password entries in rockyou.txt
+            contents = f.read()
+            pwds = contents.split()
+            pwdlist= [str(value) for value in pwds]
+            print(pwdlist)
+            for fx in pwdlist:
+                try:
+                    zf.extractall("../EncryptedFilePack", pwd=fx.encode())
+                    print(fx.encode()) 
+                    print(f"Successfully extracted files with password: {fx}")
+                    return True
+                except RuntimeError:
+                    pass
+            return False
+
+
+            # Attempt to extract the zip file using each password
+
+            # Handle correct password extract versus incorrect password attempt)
+
+    #print("[+] Password not found in list")
+
+if __name__ == "__main__":
+    main()
